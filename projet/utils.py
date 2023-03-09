044953c1365866a6143432a36405013a50575aa4
LionelDal <lionel@dalmau-fr.com>
2023-03-09 18:51:43 +0100

    Avec le fichier c'est mieux

---
+++ projet/utils.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+import platform
+
+
+def get_sub_sys():
+    """
+    As pathway are different in each computer, search for sub-sytem type
+
+    Returns
+    -------
+    sys[0] : str
+        Name of sub-system.
+
+    """
+    sys = platform.uname()  # Collect system data
+    return sys[0]
+
+def get_path():
+    """
+    As pathway are different in each computer, compute actual pathway to store data in
+    a known path
+
+    Returns
+    -------
+    path : str
+        Path of the dataset download.
+
+    """
+    path = os.getcwd()
+    path = os.path.join(path, "env")
+
+    return path  # Collect the path
