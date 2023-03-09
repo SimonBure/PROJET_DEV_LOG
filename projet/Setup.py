14a125be759cd23dedd8f285486c5b144da67500
LionelDal <lionel@dalmau-fr.com>
2023-03-09 19:35:16 +0100

    Add dir to path finding lib

--- projet/Setup.py
+++ projet/Setup.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 import utils
+import sys
+import os
+
+test = input("First time launching setup ? (Y/N)")
+if test == "Y" :
+    sys.path.append(os.getcwd())
 
 # Generate environement of the program
 
