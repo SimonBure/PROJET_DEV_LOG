a778c33800aef8a5df6baf7c9f511fbcf9a86ce9
LionelDal <lionel@dalmau-fr.com>
2023-03-14 12:40:00 +0100

    Change get_path in utils to create the env in the setup folder(previously in utils folder)

--- projet/Setup_dev.py
+++ projet/Setup_dev.py
@@ -7,24 +7,23 @@ import os
 import wget
 import zipfile
 
+env_path = os.path.dirname(os.path.realpath(__file__))
+
 
 test = input("Need to download file ? (Y/N)")
 if test == "Y":
-    add_path = utils.get_path("Other")
-    add_path = os.path.join(add_path, "temp", "list_attr_celeba.txt")
+    add_path = os.path.join(env_path, "temp", "list_attr_celeba.txt")
     url = "https://filesender.renater.fr/download.php?token=80050e2e-f52b-44ed-8bad-ff4d77649cb3&files_ids=22772324"
     wget.download(url, add_path)
     url2 = "https://filesender.renater.fr/download.php?token=80050e2e-f52b-44ed-8bad-ff4d77649cb3&files_ids=22772325"
-    add_path = utils.get_path("Other")
-    add_path = os.path.join(add_path, "temp", "img_align_celeba.zip")
+    add_path = os.path.join(env_path, "temp", "img_align_celeba.zip")
     wget.download(url2, out=add_path)
 
 
 test = input("First time launching setup ? (Y/N)")
 if test == "Y":
-    sys.path.append(utils.get_path("Other"))
-    add_path = utils.get_path("Other")
-    add_path = os.path.join(add_path, "src")
+    sys.path.append(env_path)
+    add_path = os.path.join(env_path, "src")
     sys.path.append(add_path)
 
 
@@ -36,22 +35,19 @@ if test == "Y":
     utils.create_folders()
 
     # Temp as no option for downloading dataset exist
-    path = utils.get_path("Other")
-    path = os.path.join(path, "temp", "list_attr_celeba.txt")
-    dst = utils.get_path("Img")
+    path = os.path.join(env_path, "temp", "list_attr_celeba.txt")
+    dst = utils.get_path(env_path, "Img")
     dst = os.path.join(dst, "celeba", "list_attr_celeba.txt")
     shutil.copy(path, dst)
-
-    path = utils.get_path("Other")
-    path = os.path.join(path, "temp", "img_align_celeba.zip")
-    dst = utils.get_path("Img")
+    path = os.path.join(env_path, "temp", "img_align_celeba.zip")
+    dst = utils.get_path(env_path, "Img")
     dst = os.path.join(dst, "celeba")
     with zipfile.ZipFile(path, 'r') as zip_ref:
         zip_ref.extractall(dst)
 
     test = input("Créer database ? (Y/N)")
     if test == "Y":
-        db.create_database()
+        db.create_database(env_path)
 
         img = db.request_data_by_id(1)
         print(img)
@@ -60,4 +56,4 @@ if test == "Y":
 test2 = input("Détruire environnemnt ? (Y/N)")
 
 if test2 == "Y":
-    utils.remove_env_prog()
+    utils.remove_env_prog(env_path)
