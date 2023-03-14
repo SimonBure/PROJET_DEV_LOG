fa74934dee4985549a271b779ebc2e1b15e577a1
LionelDal <lionel@dalmau-fr.com>
2023-03-14 10:37:06 +0100

    Update setup for packaging again

--- projet/src/setup.py
+++ projet/src/setup.py
@@ -5,13 +5,13 @@ setuptools.setup(
 	version ="0.0.1",
 	author = "Simon Buré, Lionel Dalmau, Mayoran Raveendran, Olivia Seffacene, Jesus Uxue",
 	author_email = "lionel.dalmau@insa-lyon.fr",
-	description = "Test of project deployement",
-	long_description = "Test of deployement for devloppement software project",
+	description = "Library for identikit program",
+	long_description = "Work in progress",
 	long_description_content_type = "text/markdown",
 	packages = setuptools.find_packages(),
 	classifiers = [
 	"Programming Language :: Python :: 3 ",
 	"Operating System :: OS Independent",
 	],
-	install_requires = ["tk", "sqlite3", "torch", "sklearn"]
+	install_requires = ["tk","sqlite3","torch","sklearn","matplotlib", "PIL"]
 	)
