2bcad9e8e65324ad2d778990a3b1ab995c261cae
Fallog <fallog@mailo.com>
2023-03-22 10:01:51 +0100

    Modification of the encoder
    Adding stride=2, padding=1 as argument for the last nn.Conv2d
    Without it, the dimension of the encoded img is greater than base dim

--- projet/src/autoencoder.py
+++ projet/src/autoencoder.py
@@ -15,28 +15,33 @@ norm_faces = faces.data.astype('float32')/255
 x_train, x_test = train_test_split(norm_faces, test_size=0.3, random_state=42)
 #x_train = faces.images[:249]
 #x_test = faces.images[250:]
-transform=transforms.ToTensor()
-revtrans=transforms.ToPILImage()
+transform = transforms.ToTensor()
+revtrans = transforms.ToPILImage()
+
 
 class Autoencoder(nn.Module):
     def __init__(self):
         super().__init__()
         # N, 1, 28, 28
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=2, padding=1), # -> N, 16, 14, 14
+            nn.Conv2d(1, 16, 3, stride=2, padding=1),  # -> N, 16, 14, 14
             nn.ReLU(),
-            nn.Conv2d(16, 32, 3, stride=2, padding=1), # -> N, 32, 7, 7
+            nn.Conv2d(16, 32, 3, stride=2, padding=1),  # -> N, 32, 7, 7
             nn.ReLU(),
-            nn.Conv2d(32, 64, 7) # -> N, 64, 1, 1
+            nn.Conv2d(32, 64, 7, stride=2, padding=1)  # -> N, 64, 1, 1
         )
 
         # N , 64, 1, 1
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, 7), # -> N, 32, 7, 7
+            nn.ConvTranspose2d(64, 32, 7),  # -> N, 32, 7, 7
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1), # N, 16, 14, 14 (N,16,13,13 without output_padding)
+            # N, 16, 14, 14 (N,16,13,13 without output_padding)
+            nn.ConvTranspose2d(32, 16, 3, stride=2,
+                               padding=1, output_padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1), # N, 1, 28, 28  (N,1,27,27)
+            # N, 1, 28, 28  (N,1,27,27)
+            nn.ConvTranspose2d(16, 1, 3, stride=2,
+                               padding=1, output_padding=1),
             nn.Sigmoid()
         )
 
@@ -44,6 +49,7 @@ class Autoencoder(nn.Module):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
+
 
 def encode(model, img):
     """
@@ -55,10 +61,11 @@ def encode(model, img):
     Returns:
         torch.Tensor: Tensor representing the encoded representation of the input image.
     """
-    transform=transforms.ToTensor()
+    transform = transforms.ToTensor()
     timg = transform(img)
     recon = model.encoder(timg)
     return recon
+
 
 def decode(model, tensor):
     """
@@ -70,7 +77,7 @@ def decode(model, tensor):
     Returns:
         PIL.Image: PIL Image object representing the decoded image.
     """
-    rev_transform=transforms.ToPILImage()
+    rev_transform = transforms.ToPILImage()
     decoded_tensor = model.decoder(tensor)
     img = rev_transform(decoded_tensor)
     return img
@@ -78,11 +85,12 @@ def decode(model, tensor):
 
 def overfitting(x_train, x_test, num_epoch):
     list_train, list_test = [], []
-    model=Autoencoder()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+    model = Autoencoder()
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=1e-3, weight_decay=1e-5)
     for epoch in range(num_epoch):
         for img in x_train:
-            timg=transform(img)
+            timg = transform(img)
             recon = model(timg)
             loss_train = criterion(recon, timg)
             optimizer.zero_grad()
@@ -90,7 +98,7 @@ def overfitting(x_train, x_test, num_epoch):
             optimizer.step()
         list_train.append(loss_train.item())
         for img in x_test:
-            timg=transform(img)
+            timg = transform(img)
             recon = model(timg)
             loss_test = criterion(recon, timg)
             optimizer.zero_grad()
@@ -98,9 +106,10 @@ def overfitting(x_train, x_test, num_epoch):
             optimizer.step()
         list_test.append(loss_test.item())
     #print(list_train, list_test)
-    plt.plot(range(num_epoch),list_train,color="red")
-    plt.plot(range(num_epoch),list_test,color="blue")
+    plt.plot(range(num_epoch), list_train, color="red")
+    plt.plot(range(num_epoch), list_test, color="blue")
     plt.savefig(path+"/overfit.png")
+
 
 def training(data, num_epochs):
     """
@@ -113,10 +122,11 @@ def training(data, num_epochs):
     """
     model = Autoencoder()
     criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=1e-3, weight_decay=1e-5)
     for epoch in range(num_epochs):
         for img in data:
-            timg=transform(img)
+            timg = transform(img)
             recon = model(timg)
             loss = criterion(recon, timg)
             optimizer.zero_grad()
@@ -125,7 +135,8 @@ def training(data, num_epochs):
         print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
     return model
 
-def save_model(model,name_file):
+
+def save_model(model, name_file):
     """
     Saves the state dictionary of a PyTorch model to a file with the given name.
 
@@ -134,6 +145,7 @@ def save_model(model,name_file):
         name_file (str): Filename to save the model to.
     """
     torch.save(model.state_dict(), name_file)
+
 
 def load_model(name_file):
     """
@@ -151,6 +163,7 @@ def load_model(name_file):
     model.load_state_dict(torch.load(name_file))
     return model
 
+
 def save_fig(im, path, img_type):
     """
     Saves a matplotlib image object to a file in the specified path.
@@ -162,6 +175,7 @@ def save_fig(im, path, img_type):
     plt.imshow(im)
     plt.savefig(os.path.join(path, img_type))
 
+
 def save_encoded_im(tensor, name_file):
     """
     Saves a PyTorch tensor to a file with the given name.
@@ -171,6 +185,7 @@ def save_encoded_im(tensor, name_file):
         name_file (str): The filename to save the tensor to.
     """
     torch.save(tensor, name_file)
+
 
 def load_encoded_im(name_file):
     """
@@ -187,10 +202,10 @@ def load_encoded_im(name_file):
 
 
 def launch_encoder(env_path):
-    path=utils.get_path(env_path,"Encoder")
+    path = utils.get_path(env_path, "Encoder")
 
-    flag=0
-    if flag==0:
+    flag = 0
+    if flag == 0:
 
         model = training(faces.images, 10)
         enco_im = encode(model, faces.images[0])
@@ -206,13 +221,12 @@ def launch_encoder(env_path):
         plt.savefig(path+"/modif_im.png")
         """
 
-        deco_im=decode(model, enco_im)
+        deco_im = decode(model, enco_im)
         save_fig(deco_im, path, "recon_im.png")
 
-    if flag==1:
+    if flag == 1:
         overfitting(x_train, x_test, 30)
 
 
 if __name__ == '__main__':
     launch_encoder("../")
-
