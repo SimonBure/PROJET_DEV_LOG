6dcdeb9e554f4ed42326ddf89efbf1dc4111543b
LionelDal <lionel@dalmau-fr.com>
2023-03-27 11:53:53 +0200

    Version that alow the 2 versions of the program

--- projet/Setup_dev.py
+++ projet/Setup_dev.py
@@ -11,17 +11,6 @@ import zipfile
 
 env_path = os.path.dirname(os.path.realpath(__file__))
 
-
-test = input("Need to download dataset ? (Y/N)")
-if test == "Y" or test == "y":
-    add_path = os.path.join(env_path, "temp", "list_attr_celeba.txt")
-    url = "https://filesender.renater.fr/download.php?token=80050e2e-f52b-44ed-8bad-ff4d77649cb3&files_ids=22772324"
-    wget.download(url, add_path)
-    url2 = "https://filesender.renater.fr/download.php?token=80050e2e-f52b-44ed-8bad-ff4d77649cb3&files_ids=22772325"
-    add_path = os.path.join(env_path, "temp", "img_align_celeba.zip")
-    wget.download(url2, out=add_path)
-
-
 test = input("First time launching setup ? (Y/N)")
 if test == "Y" or test == "y":
     sys.path.append(env_path)
@@ -34,8 +23,29 @@ if test == "Y" or test == "y":
 test = input("Créer environnement ? (Y/N)")
 
 if test == "Y" or test == "y":
-    utils.create_folders(env_path)
-
+    utils.create_folders(env_path, dev = True)
+    
+    # idkit.db database du programme
+    path = utils.get_path(env_path, "Database")
+    add_path = os.path.join(path, "idkit.db")
+    url = "https://filesender.renater.fr/download.php?token=e08cc673-d83a-45c8-a7ed-cd924c3f92e5&files_ids=23283443"
+    wget.download(url, add_path)
+    
+    # idkit.png pour le logo du programme
+    path = utils.get_path(env_path, "Interface")
+    url2 = "https://filesender.renater.fr/download.php?token=e08cc673-d83a-45c8-a7ed-cd924c3f92e5&files_ids=23283444"
+    add_path = os.path.join(env_path, "idkit.png")
+    wget.download(url2, out=add_path)
+    
+    # new_dataset.zip pour les images de la base de données
+    path = utils.get_path(env_path, "Img_base")
+    add_path = os.path.join(path, "new_dataset.zip")
+    url3 = "https://filesender.renater.fr/download.php?token=e08cc673-d83a-45c8-a7ed-cd924c3f92e5&files_ids=23283445"
+    wget.download(url3, out=add_path)
+    with zipfile.ZipFile(add_path, 'r') as zip_ref:
+        zip_ref.extractall(path)
+    
+    
     # Temp as no option for downloading dataset exist
     path = os.path.join(env_path, "temp", "list_attr_celeba.txt")
     dst = utils.get_path(env_path, "Img_base")
